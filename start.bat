1b3f6d28c6bc35bd9e094fab95f1aa6852247020
Eddy Su <eddysu123@gmail.com>
2025-07-23 19:12:41 -0400

    Updated start.bat
    the commend 'node server.js' blocks any furhter execution, so a new cmd window needed to be opened to handle the server while the index will be opened afterwards

--- start.bat
+++ start.bat
@@ -1,14 +1,12 @@
 @echo off
 echo Starting backend server...
 
-REM Start the backend server 
-cd backend
-node server.js
+REM Start the backend server in a new window
+start "" cmd /k "cd backend && node server.js"
 
 echo Opening frontend in default browser...
 
 REM Open the HTML file in the default browser
-cd ..
 start "" "frontend\index.html"
 
 echo Done.
